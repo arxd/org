d642a416e72a63fad397fe0598f371d52779715f
aaron <aaronwdavidson@gmail.com>
2017-01-01 21:07:47 +0900

    Just Thinking

---
+++ src/money/data.py
@@ -0,0 +1,66 @@
+jjj"""
+        We are interested in how our value is moving around, going up, going
+        down. But, we can only track what quantities we have.
+
+        I have a house.
+        I have $3000 in the bank.
+        I have 4 shares of Apple Stock
+        I have 2.1 bitcoin
+        I have 1 acre of land
+        I have 12 eggs
+
+        
+        The _value_ of the things that you have is variable.
+        If you are stranded in the middle of a desert only one of those things
+        has any value.
+
+        Inflation means your $3000 is becoming less valuable over time, but
+        your Apple Stock might become more valuable over time.  The eggs have
+        a pretty constant value (for a short period of time).
+
+        The problem with value is that it does not obey conservation laws.  For
+        example, gravity obeys conservation laws.  If you go for bike ride you
+        might ride up hill or downhill, but when you return home you are
+        guaranteed that you rode uphill exactly as much as you got to go
+        downhill.
+
+        Value doesn't obey that law (it should), this is one of the reasons I
+        dont like economics.  You can trade things around in a circle and you
+        will usually come out with less value than when you started.  If you
+        are very clever you might get back _more_ value than when you started.
+        That is very irritating.
+
+
+        Value is created:
+            * Working at a job
+            * Building a table
+
+        Value is destroyed:
+            * Blowing up your car
+            * Eating food
+
+        Value takes on many forms:
+            * Gold
+            * Real Estate
+            * Yen
+            * Stock
+            * Etc.
+
+        So we have to keep track of the things we have, and then work out how
+        much value that is.  How much value is it? 
+        Value can only be measured by trade.  You have to compare what you have with something
+        else to assess its value.  Some demands are fairly constant (hunger) so
+        you can use those as stable reference points to calculate the values of
+        other commodities.  So how much bread can you get for your car?  That
+        is how valuable your car is.  Is there a more constant reference than
+        our appitite to measure value against?
+
+        Our Possesions may have value but there may be barriers to trade.  It
+        is easy to trade money in the bank to cash in your hand.  It is hard to
+        trade your house into cash in your hand.  This barrier is dependant
+        on the two tradeables.
+
+
+"""
+
+

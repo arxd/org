346a3bed47336319e24bd7730c565fe6318e4ed5
aaron <aaronwdavidson@gmail.com>
2017-04-07 11:00:50 +0900

    Moved tag handling to Tag class

--- src/Food.py
+++ src/Food.py
@@ -1,10 +1,10 @@
 """
 There are two classes:  Food and Ingredient.
 A Food can be a recipe (Cookies) or a basic food (Apple, Cinnamon).
-An Ingedient is an amount of food (in grams).  
+An Ingedient is an amount of food (in grams).
 """
 
-from PlainTxtDB import YAMLSetter
+from PlainTxtDB import YAMLSetter, Tag
 
 class Ingredient(YAMLSetter):
 	""" An ingredient is a certain amount of food, prepared in a certain way.
@@ -16,18 +16,20 @@ class Ingredient(YAMLSetter):
 	yaml_tag = "!Ingredient"
 	yaml_props = {
 		'food': None,
-		'amount': 1.0,
+		'amount': 0.0,
+		'unit':'',
 		'prep':''
 	}
 	
-	volume_units = [('cup', 236.588), ('tbsp', 14.7868), ('tsp',4.9289), ('floz',29.5735), ('ml',1.0)]
-	mass_units = [('oz',28.3495), ('g',1.0)]
-		
+	VOLUME_UNITS = ['cup', 'tbsp', 'tsp', 'floz', 'ml']
+	MASS_UNITS = ['oz', 'g']
+	UNITS = {'cup':236.588, 'tbsp':14.7868, 'tsp':4.9289, 'floz':29.5735, 'ml':1.0, 'oz':28.3495, 'g':1.0}
+	
 	def __init__(self, **kwargs):
 		YAMLSetter.__init__(self, kwargs)
 
 	def __mul__(self, factor):
-		return Ingredient(food=self.food, amount=self.amount*factor, prep=self.prep)
+		return Ingredient(food=self.food, amount=self.amount*factor, unit = self.unit, prep=self.prep)
 
 	def __imul__(self, factor):
 		self.amount *= factor
@@ -36,53 +38,76 @@ class Ingredient(YAMLSetter):
 	def __eq__(self, other):
 		return self.food.name == str(other)
 
-	def guess_unit(self):
-		""" Try to guess the best unit to represent this amount in.  There are hints if unit_volume or unit_mass
-		is set to 1.0.
+	def all_units(self):
+		return [self.food.unit_label] + Ingredient.VOLUME_UNITS + Ingredient.MASS_UNITS
+		
+	def set(self, amt):
+		""" Set the amount with a string like '1 2/3 cup' or '33.5 g'
+		If amt is a number (float/int) then just set the amout without setting the units.
 		"""
-		if self.food.unit_volume == 1.0: # a volume unit would be best
-			guess_units = [x[0] for x in Ingredient.volume_units]
-		elif self.food.unit_mass == 1.0: # a mass unit would be best
-			guess_units = [x[0] for x in Ingredient.mass_units]
-		else: # try volume or unit_label fall back on mass
-			guess_units = [self.food.unit_label] + [x[0] for x in Ingredient.volume_units] 
-		
-		# The units are listed in the order of priority so the first one you find wins
-		for v,u in  [self.amt(t) for t in guess_units]:
-			# Are we close to 1/2 1/3 1/4 etc. of a unit?
-			if (v/0.25 + 0.02)%1.0 < 0.04 or (v/ (1.0/3.0) + 0.02)%1.0 < 0.04:
-				return u
-		return u
-		
+		if isinstance(amt, str):
+			amt = list(filter(lambda x: x, amt.split(' ')))
+			if len(amt) == 1: # units
+				value = float(amt[0])
+				unit = ''
+			elif len(amt) == 2 and '/' in amt[0]: # frac unit
+				a, b = amt[0].split('/')
+				value = float(a) / float(b)
+				unit = amt[1]
+			elif len(amt) == 2 and '/' in amt[1]: # whole frac
+				a, b = amt[1].split('/')
+				value = float(amt[0]) + float(a) / float(b)
+				unit = ''
+			elif len(amt) == 2: # whole unit
+				value = float(amt[0])
+				unit = amt[1]
+			elif len(amt) == 3: # whole frac units
+				a, b = amt[1].split('/')
+				value = float(amt[0]) + float(a) / float(b)
+				unit = amt[2]
+			else:
+				raise Exception("Invalid amount %s"%amt)
+				
+			if unit not in self.all_units():
+				raise Exception("Unknown units '%s'"%unit)
+			
+			self.amount = value
+			self.unit = unit
+		else:
+			self.amount = float(amt)
+
 
 	def amt(self, unit=None):
-		""" Return a tuple (amt, 'unit') of the amount of this ingredient.  
-		If you leave unit=None then it tries to guess the best unit.
+		""" Return the amount of this ingredient in units of \a unit.  
+		If you leave unit=None then it just returns the value in the current units.
 		"""
-		if unit == None:
-			unit = self.guess_unit()
+		if not unit or unit == self.unit:
+			return self.amount
 			
-		# Is the unit a custom food unit (1 egg, 1 stick of butter, 1 carrot)
-		if unit == self.food.unit_label:
-			return (self.amount / self.food.unit_mass, unit)
-			
-		# Check volume units
-		for u,v in Ingredient.volume_units:
-			if u == unit:
-				return (self.amount * self.food.unit_volume / self.food.unit_mass / v, unit)
-				
-		# Maybe it is a mass unit
-		for u,v in Ingredient.mass_units:
-			if u == unit:
-				return (self.amount / v, unit)
-		
-		# Couldn't find the unit
-		raise Exception("%s is not a valid unit"%unit)
-	
+		if unit == 'g':
+			if self.unit in Ingredient.VOLUME_UNITS:
+				return self.amount * Ingredient.UNITS[self.unit] * self.food.unit_mass / self.food.unit_volume
+			elif self.unit in Ingredient.MASS_UNITS:
+				return self.amount * Ingredient.UNITS[self.unit]
+			else:
+				return self.amount * self.food.unit_mass
+		else:
+			grams = self.amt('g') # first convert to grams
+			if unit in Ingredient.VOLUME_UNITS:
+				return grams * self.food.unit_volume / self.food.unit_mass / Ingredient.UNITS[unit]
+			elif unit in Ingredient.MASS_UNITS:
+				return grams / Ingredient.UNITS[unit]
+			elif unit == self.food.unit_label:
+				return grams / self.food.unit_mass
+			else:
+				raise Exception("Unknown unit <%s>"%unit)
+
 	def str_amt(self, unit=None):
-		""" Convert the floating point amount 1.3333 to a nice 1-1/3
+		""" Convert the floating point amount 5.3333 to a nice 5 1/3
 		"""
-		amt, unit = self.amt(unit)
+		if not unit:
+			unit = self.unit
+		amt = self.amt(unit)
 		whole, part = divmod(amt, 1)
 		if part > 0.875:
 			whole += 1
@@ -101,7 +126,7 @@ class Ingredient(YAMLSetter):
 			part = ""
 
 		whole = str(int(whole)) if int(whole) else ""		
-		return "%s%s%s %s"%(whole, '-' if whole and part else '', part, unit)
+		return "%s%s%s %s"%(whole, ' ' if whole and part else '', part, unit)
 	
 	def __str__(self):
 		return self.food.name
@@ -147,6 +172,23 @@ class Food(YAMLSetter):
 	def __eq__(self, other):
 		return self.name == str(other)
 	
+	def has_tag(self, tagname):
+		return tagname in self.tags
+		
+	def add_ingredient(self, food, amt_str='0.0 g', prep=""):
+		ig = Ingredient(food=food, prep=prep)
+		ig.set(amt_str)
+		self.ingredients.append(ig)
+		self.add_tag('recipe')
+	
+	def add_tag(self, tagname):
+		for i in Tag.OPS + ['(', ')']:
+			if i in tagname:
+				raise Exception("'%s' cannot be in a tagname"%i)
+		tagname = tagname.upper()
+		if tagname not in self.tags:
+			self.tags.append(tagname)
+	
 	def scale(self, factor):
 		""" Scale this recipe by the given factor """
 		for i in self.ingredients:
@@ -159,13 +201,14 @@ class Food(YAMLSetter):
 	def verbose(self):
 		title = "%s %s\n"%(self.name, ' : '+self.description if self.description else '')
 		astr = '='*len(title) + '\n%s'%title + '-'*len(title)
+		if self.tags:
+			astr += '\n<' + '> <'.join(self.tags) + '>\n'
 		if self.ingredients:
 			astr += "\nIngredients:\n"
 			max_name = max(map(lambda x: len(str(x)), self.ingredients))
-			astr += '\n'.join(map(lambda s: '   %s%s | %s'%(' '*(max_name-len(str(s))), s, s.str_amt()), self.ingredients))
+			astr += '\n'.join(map(lambda s: '   %s%s | %s   %s'%(' '*(max_name-len(str(s))), s, s.str_amt(), s.prep), self.ingredients ))
 		if self.instructions:
-			astr += "\n\nInstructions:\n"
-			astr += '\n'.join(map(lambda s: '  * '+s, self.instructions))
+			astr += '\n\n' + '\n'.join(map(lambda s: '  * '+s, self.instructions)) + '\n'
 	
 		return astr
 		

a73496ff0aac22a265ba9e98e71ba0e27f677a13
aaron <aaronwdavidson@gmail.com>
2017-04-07 10:59:50 +0900

    Created the Tag class for generic tag handling

--- src/PlainTxtDB.py
+++ src/PlainTxtDB.py
@@ -12,6 +12,102 @@ from datetime import datetime
 import os.path
 import os
 
+
+class Tag(object):
+	AND = '&'
+	OR = ','
+	NOT = '-'
+	OPS = [ AND, OR, NOT]
+
+	@staticmethod
+	def parse(tagstr):
+		""" Turn a sloppy infix string  '(my car, your car, -(bob , cob)) & expensive'
+		into a nice valid tagexpr ['&', ',', 'MY CAR', ',', 'YOUR CAR', '-', ',', 'BOB', 'COB', 'EXPENSIVE']
+		"""
+		if not tagstr.strip():
+			return []
+		tagstr += '(' # add a ( to remove specail cases at the end of the string
+		expr = []
+		stack = []
+		i = 0
+		
+		while i < len(tagstr):
+			if len(expr) > 1 and expr[-1] not in Tag.OPS and expr[-2] == Tag.NOT:
+				expr[-1] = [Tag.NOT, expr.pop()]
+				i -= 1
+			elif len(expr)>1 and expr[-1] not in Tag.OPS and expr[-2] == Tag.AND:
+				a,b,c = (expr.pop(), expr.pop(), expr.pop())
+				expr.append([b,c,a])
+				i -= 1
+			elif tagstr[i] == '(':
+				stack.append(expr)
+				expr = []
+			elif tagstr[i] == ')':
+				stack[-1].append(expr[0] if len(expr) == 1 else expr)
+				expr = stack.pop()
+			elif tagstr[i] in Tag.OPS:
+				expr.append(tagstr[i])
+			elif tagstr[i] != ' ':
+				expr.append(tagstr[i])
+				while tagstr[i+1] not in ['(', ')']+Tag.OPS :
+					expr[-1] += tagstr[i+1]
+					i += 1
+				expr[-1] = expr[-1].strip().upper()
+			i += 1
+			
+		def flatten(lst):
+			out = [Tag.OR] * (0 if lst[0] in Tag.OPS else len(lst)//2)
+			for i in range(0, len(lst), 2 if lst[0] not in Tag.OPS else 1):
+				out += [lst[i]] if type(lst[i]) == str else flatten(lst[i])
+			return out
+		
+		return flatten(stack[0])
+
+
+	@staticmethod
+	def match(obj, tagexpr):
+		""" tag expr has a special prefix form.  And the tag names must be all caps.
+		If tagexpr is a string then it passes the string through parse for you.
+		obj must have the has_tag() method.
+		 
+		['BOB']
+		['-', 'BOB']
+		['&', 'BOB', ',', '-', 'MY HOUSE', 'MY CAR']
+		"""
+		if isinstance(tagexpr, str):
+			tagexpr = Tag.parse(tagexpr)
+		
+		if not tagexpr:
+			return True
+		
+		def match_r(i):
+			if tagexpr[i] == Tag.AND or tagexpr[i] == Tag.OR:
+				bool1, end1 = match_r(i+1)
+				bool2, end2 = match_r(end1)
+				return ( (bool1 and bool2) if tagexpr[i] == Tag.AND else (bool1 or bool2), end2)
+			elif tagexpr[i] == Tag.NOT:
+				bool, end = match_r(i+1)
+				return (not bool, end)
+			else:
+				if obj.has_tag(tagexpr[i]):
+					return (True, i+1)
+				return (False, i+1)
+
+		return match_r(0)[0]
+
+	@staticmethod
+	def filter(iterable, tagstr):
+		""" Return a filtered list of foods based on the tagstr.
+		This is prefered because it only parses the tagstr once.
+		"""
+		try:
+			tagexpr = Tag.parse(tagstr)
+		except Exception as e:
+			print("Syntax Error in tag expression")
+			return []
+		return [f for f in iterable if Tag.match(f, tagexpr)]
+
+
 class YAMLSetter(yaml.YAMLObject):
 	def __init__(self, kwargs):
 		for k,v in self.__class__.yaml_props.items():
